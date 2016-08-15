e7cb4fa6b55bfd4721a3aa54d3077f6517a4adf1
Beatrice Licciardello <beatrice.licciardello@bbc.co.uk>
2016-08-15 22:12:43 +0100

    Hacker Rank Java BigNumber Java BigInteger

---
+++ Java/BigNumber/JavaBigInteger.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner cin=new Scanner(System.in);
+		BigInteger a=cin.nextBigInteger(),b=cin.nextBigInteger();
+		System.out.println(a.add(b));
+		System.out.println(a.multiply(b));
+	}
+}
