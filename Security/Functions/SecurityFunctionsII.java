5ce45087126b3ed2512e22abd38b7bb1558dfb4a
Beatrice Licciardello <beatrice.licciardello@bbc.co.uk>
2016-08-25 15:32:11 +0100

    Hacker Rank Security Functions Security Functions II

---
+++ Security/Functions/SecurityFunctionsII.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		System.out.println((int)Math.pow(x, 2));
+
+		sc.close();
+	}
+}
